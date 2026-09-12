1e2f21f42d174c16049ce6385da8f0561c02e15c
cingopayout-hash <cingopayout@gmail.com>
2026-09-12 11:46:30 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/motionframe-ai/', // <-- TAMBAHKAN BARIS INI (sesuai nama repo)
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/motionframe-ai/',
 })
